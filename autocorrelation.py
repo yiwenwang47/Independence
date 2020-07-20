ab004c674c2573a24486aa9a003e4f1948a43418
yiwenwang9702 <yiwenwang9702@gmail.com>
2020-07-19 22:52:49 -0400

    started autocorrelation

---
+++ autocorrelation.py
@@ -0,0 +1,13 @@
+# Namely, spatial autocorrelation tests.
+
+import numpy as np
+
+# Moran's I
+def Moran_I(array: np.ndarray, matrix: np.ndarray) -> np.float:
+
+    return 
+
+# Geary's C
+def Geary_C(array: np.ndarray, matrix: np.ndarray) -> np.float:
+
+    return
